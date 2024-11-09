ab5313d1c2bea92eb62f9f948063576bced0f8bc
Giovanni Ricca <giovanniricca@proton.me>
2024-11-09 13:50:08 +0100

    extra: Initial commit
    Change-Id: Ic29aaad07dc6c45aca91c6b816a246be1cf3d504

---
+++ gen_los_ota.py
@@ -0,0 +1,52 @@
+import sys
+import os
+from hashlib import md5
+from re import search, sub
+from glob import glob
+
+def getprop(prop):
+    return search(
+        r"".join(["(?<=", prop, "=).*"]), open("system/build.prop").read()
+    ).group(0)
+
+
+version, datetime, incremental, codename = (
+    getprop("ro.lineage.build.version"),  # version
+    getprop("ro.build.date.utc"),  # datetime
+    getprop("ro.build.version.incremental"),  # incremental
+    getprop("ro.lineage.device"),  # codename
+)
+filename = max(
+    glob("".join(["lineage-", version, "*", ".zip"])),
+    key=os.path.getctime,
+)
+id = md5(open(filename, "rb").read()).hexdigest()
+size = os.stat(filename).st_size
+url = "".join(
+    [
+        # TO EDIT
+        "https://github.com/ItsVixano-releases/LineageOS_",
+        codename,
+        "/releases/download/",
+        sub("[^0-9]", "", incremental)[:-6],
+        "/",
+        filename,
+    ]
+)
+
+ota = f"""{{
+  "response": [
+    {{
+      "datetime": {datetime},
+      "filename": "{filename}",
+      "id": "{id}",
+      "romtype": "unofficial",
+      "size": {size},
+      "url": "{url}",
+      "version": "{version}"
+    }}
+  ]
+}}
+"""
+
+print(ota)
---
+++ product.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
